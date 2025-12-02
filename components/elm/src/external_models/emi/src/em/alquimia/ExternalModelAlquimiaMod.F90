1520e3ba7b15728979ca1481660cc01b1aa128cb
Bailey Murphy <42819353+bamurphy5@users.noreply.github.com>
2025-12-02 18:06:21 -0500

    ...forgot to close ()

--- components/elm/src/external_models/emi/src/em/alquimia/ExternalModelAlquimiaMod.F90
+++ components/elm/src/external_models/emi/src/em/alquimia/ExternalModelAlquimiaMod.F90
@@ -2727,7 +2727,7 @@ subroutine run_vert_transport(this,actual_dt, total_mobile, free_mobile, &
         endif
         ! Approximate ebullition by increasing diffusion coefficient when gas pressure is higher than water pressure
         ! BAM 12/2/25: editing so ebullition only approximated when pores contain mostly liq. water (frozen ground control), adding liq_frac(j) gating to help w/CH4 release in winter
-        if(gas_pressure>water_pressure .and. sat(j)>0.75) .and. liq_frac(j)>0.7_r8 then
+        if(gas_pressure>water_pressure .and. sat(j)>0.75 .and. liq_frac(j)>0.7_r8) then
           diffus(j) = max(diffus(j),2.0e-5_r8*0.66_r8*porosity(j)*((gas_pressure-water_pressure)/gas_pressure)*((gas_pressure-water_pressure)/gas_pressure)**3)
         endif
         atmo_pressure = 101.325e3_r8 ! Pa
